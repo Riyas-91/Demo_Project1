bb0e239a49beb1e47c1dd5378962c5dadab73f3f
Riyas <riyas1226riyas@gmail.com>
2021-11-30 23:06:07 +0530

    Demo_Project

---
+++ src/demo_package/Push_Github.java
@@ -0,0 +1,19 @@
+package demo_package;
+
+public class Push_Github {
+
+	public static void main(String[] args) {
+		System.out.println("Github Pushed Successfully");
+		System.out.println("------------------------");
+		String s = "Github";
+		
+		if(s =="Github") {
+			System.out.println("Code to be Pushed Github");
+		}
+		else {
+			System.out.println("Stay Remained");
+		}
+
+	}
+
+}
